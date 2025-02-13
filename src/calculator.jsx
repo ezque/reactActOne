8dd597fcdb7af3715b009320514c43d44ca5a88f
ezque <vonmauzar46@gmail.com>
2025-02-13 14:47:43 +0800

    Updated project with latest changes

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -26,25 +26,25 @@ const Calculator = () => {
             <div>
                 <input type="text" value={expression} disabled />
             </div>
-            <div>
+            <div class="rows">
                 <button onClick={() => handleInput('1')}>1</button>
                 <button onClick={() => handleInput('2')}>2</button>
                 <button onClick={() => handleInput('3')}>3</button>
                 <button onClick={() => handleInput('+')}>+</button>
             </div>
-            <div>
+            <div class="rows">
                 <button onClick={() => handleInput('4')}>4</button>
                 <button onClick={() => handleInput('5')}>5</button>
                 <button onClick={() => handleInput('6')}>6</button>
                 <button onClick={() => handleInput('-')}>-</button>
             </div>
-            <div>
+            <div class="rows">
                 <button onClick={() => handleInput('7')}>7</button>
                 <button onClick={() => handleInput('8')}>8</button>
                 <button onClick={() => handleInput('9')}>9</button>
                 <button onClick={() => handleInput('*')}>*</button>
             </div>
-            <div>
+            <div class="rows">
                 <button onClick={() => handleInput('0')}>0</button>
                 <button onClick={clear}>C</button>
                 <button onClick={calculate}>=</button>
